e4f7741960bc3f42db1ea403b9bde69a2c0e0a75
kinka-men <petite69gk@gmail.com>
2025-05-08 01:11:51 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+// service-worker.js
+const CACHE_NAME = 'gp-office-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/app.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
