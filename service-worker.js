aeb22a70a54dc8b7f51ac21357ef1e93ae0e3c72
kinka-men <petite69gk@gmail.com>
2025-05-08 13:04:04 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,11 @@
 // service-worker.js
 const CACHE_NAME = 'gp-office-v1';
 const assets = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/app.js',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    'index.html',
+    'styles.css',
+    'app.js',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', event => {
